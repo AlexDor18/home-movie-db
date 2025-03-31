c267b26fe24df49ce7f7ea42349fdfe8b1c68736
Alexander Dormann <AlDo14.AD@gmail.com>
2025-03-31 18:16:58 +0200

    initialize backend

---
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/HomeMovieDbBackendApplication.java
@@ -0,0 +1,13 @@
+package org.thro.sqs.homemoviedb.home_movie_db_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMovieDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMovieDbBackendApplication.class, args);
+	}
+
+}
---
+++ home-movie-db-backend/src/test/java/org/thro/sqs/homemoviedb/home_movie_db_backend/HomeMovieDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thro.sqs.homemoviedb.home_movie_db_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMovieDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
