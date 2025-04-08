c3b6138dc82dd15200b7c7f1f72e254f0e846d49
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-08 13:45:55 +0200

    remove unused import

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/ResourceController.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/ResourceController.java
@@ -1,7 +1,6 @@
 package org.thro.sqs.homemoviedb.home_movie_db_backend.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
