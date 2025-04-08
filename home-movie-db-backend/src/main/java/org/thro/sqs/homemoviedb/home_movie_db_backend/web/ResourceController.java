6bb3ccbf733f64d4332cc8f2949426e93bfaa9c5
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-08 13:43:02 +0200

    config spring for spa serving

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/ResourceController.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/ResourceController.java
@@ -1,12 +1,15 @@
 package org.thro.sqs.homemoviedb.home_movie_db_backend.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class ResourceController {
     @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
-        return "forward:/";
+        return "forward:/index.html";
     }
+    
 }
