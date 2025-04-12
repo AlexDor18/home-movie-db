848c8dc7b0d33e188d6ff9a87483d0420c00aa9b
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-12 15:33:08 +0200

    fix api path

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/ApiConfig.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/ApiConfig.java
@@ -1,5 +1,5 @@
 package org.thro.sqs.homemoviedb.home_movie_db_backend.web;
 
 public final class ApiConfig {
-    public static final String BASE_PATH = "/api";
+    public static final String BASE_PATH = "api";
 }
--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/MoviesWeb.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/MoviesWeb.java
@@ -3,11 +3,13 @@ package org.thro.sqs.homemoviedb.home_movie_db_backend.web;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.web.models.MovieMessage;
 
 @RestController
-public record MoviesWeb() {
+@RequestMapping(ApiConfig.BASE_PATH)
+public class MoviesWeb {
 
     @GetMapping("/movies")
     public List<MovieMessage> movies() {
