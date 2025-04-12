6d05373c7b110ddfc77c3bd21b96ddc1acb37e1f
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-12 16:42:23 +0200

    Added Business Impl

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/MoviesWeb.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/MoviesWeb.java
@@ -2,23 +2,22 @@ package org.thro.sqs.homemoviedb.home_movie_db_backend.web;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.business.interfaces.PublicMovies;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.web.models.MovieMessage;
 
 @RestController
 @RequestMapping(ApiConfig.BASE_PATH)
 public class MoviesWeb {
 
+    @Autowired
+    private PublicMovies publicMovies;
+
     @GetMapping("/movies")
     public List<MovieMessage> movies() {
-        MovieMessage movieMessage = new MovieMessage();
-        movieMessage.setId(1L);
-        movieMessage.setTitle("Movie 1");
-        movieMessage.setOverview("Overview 1");
-        movieMessage.setGenres(List.of("Genre 1", "Genre 2"));
-
-        return List.of(movieMessage);
+        return publicMovies.getAllMovies();
     }
 }
