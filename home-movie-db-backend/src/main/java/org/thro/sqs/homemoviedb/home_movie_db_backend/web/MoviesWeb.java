6d0a0d2fd80e7445d8105bcb606f5660dad0cfa4
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-12 15:01:23 +0200

    init movie mock

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/MoviesWeb.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/web/MoviesWeb.java
@@ -1,13 +1,22 @@
 package org.thro.sqs.homemoviedb.home_movie_db_backend.web;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.web.models.MovieMessage;
 
 @RestController
 public record MoviesWeb() {
 
     @GetMapping("/movies")
-    public String movies() {
-        return "movies";
+    public List<MovieMessage> movies() {
+        MovieMessage movieMessage = new MovieMessage();
+        movieMessage.setId(1L);
+        movieMessage.setTitle("Movie 1");
+        movieMessage.setOverview("Overview 1");
+        movieMessage.setGenres(List.of("Genre 1", "Genre 2"));
+
+        return List.of(movieMessage);
     }
 }
