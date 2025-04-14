067812d396c512ba8e90148154d4f1eab1d61b01
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-14 13:06:34 +0200

    init frontend tests

---
+++ home-movie-db-frontend/src/tests/pages/NotFoundPageTest.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
+import { describe, expect, it } from 'vitest'
+import NotFoundPage from '../../pages/NotFoundPage'
+
+describe('NotFoundPage', () => {
+  it('renders 404 page not found', async () => {
+    render(
+      <MemoryRouter initialEntries={['/not-found']}>
+        <NotFoundPage />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByText('404 Not Found')).toBeInTheDocument()
+
+    const linkElement = await screen.getByText('Nothing found on route /not-found');
+
+    expect(linkElement).toBeInTheDocument()
+  })
+})
--- home-movie-db-frontend/vite.config.ts
+++ home-movie-db-frontend/vite.config.ts
@@ -5,5 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    reporters: ['default'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+      reportOnFailure: true
+    },
+    setupFiles: ["./src/tests/setup.ts"],
+  },
   plugins: [react(), tailwindcss()],
 })
