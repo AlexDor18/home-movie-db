4abb0af3335748c1b41d186f2bf2ab6e55eb311e
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-14 18:10:39 +0200

    feat: added footer

--- home-movie-db-frontend/src/pages/Layout.tsx
+++ home-movie-db-frontend/src/pages/Layout.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router";
 import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
 
 const Layout: React.FC = () => {
   return (
     <div className="bg-[#F0EFFF]">
       <Header />
       <Outlet />
-      <footer className="layout-footer">
-        <p>Footer content</p>
-      </footer>
+      <Footer />
     </div>
   );
 };
