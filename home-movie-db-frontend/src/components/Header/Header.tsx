e2ccb53692c27180ed2c5e0f7de17ea5139950da
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-20 17:29:21 +0200

    chore: sonar test

--- home-movie-db-frontend/src/components/Header/Header.tsx
+++ home-movie-db-frontend/src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export const Header: React.FC = () => {
+
+  const one = parseInt('1');
+
   return (
     <header className="bg-white border-b-2 border-b-[#ccc] border-b-solid px-8 py-4 shadow-md">
       <h1 className="text-xl font-bold">Home Movie DB</h1>
