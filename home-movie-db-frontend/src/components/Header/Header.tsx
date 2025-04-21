c9a892aea7e879109623c35e50f83cad46c382fc
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-21 12:44:36 +0200

    build: removed sonar check

--- home-movie-db-frontend/src/components/Header/Header.tsx
+++ home-movie-db-frontend/src/components/Header/Header.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 
 export const Header: React.FC = () => {
 
-  const one = parseInt('1');
-  if(one == 1){
-    console.log(one)
-  }
-
   return (
     <header className="bg-white border-b-2 border-b-[#ccc] border-b-solid px-8 py-4 shadow-md">
       <h1 className="text-xl font-bold">Home Movie DB</h1>
