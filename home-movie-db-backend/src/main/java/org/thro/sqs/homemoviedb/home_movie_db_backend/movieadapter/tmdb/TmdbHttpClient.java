1fd19b84c3cd7a2431c39d77e15dd0308f2aaf5b
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-21 20:52:00 +0200

    build: removed value annotation

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbHttpClient.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbHttpClient.java
@@ -2,23 +2,32 @@ package org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.tmdb;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class TmdbHttpClient {
 
-    @Value("${tmdb.apiKey}")
+    //@Value("${tmdb.apiKey}")
     private String tmdbApiKey;
 
     private RestClient restClient;
 
-    private TmdbHttpClient(){
+    public TmdbHttpClient(){
+        tmdbApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2UxODEwZTIwZDE5N2ZmYTIxNTRhM2JkNjE1NDZmNiIsIm5iZiI6MTU0NTU3NDY1My44NDgsInN1YiI6IjVjMWY5OGZkOTI1MTQxNjgwNWJmZTdhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VKSP8ZOqfQL9L8BP8mrNJ2_uLOhvEsldqbs9ikY4kLs";
         restClient = RestClient.builder().baseUrl("https://api.themoviedb.org/3").defaultHeader("Authorization", "Bearer " + tmdbApiKey).defaultHeader("Content-Type", "application/json").build();
-    
     }
 
     public <T extends Object> T get(String uri, Class<T> responseClass) {
-
-        return restClient.get().uri(uri).retrieve().body(responseClass);
+        try {
+            return restClient.get().uri(uri).retrieve().body(responseClass);
+        } catch(HttpClientErrorException err){
+            log.error(uri, err);
+            throw err;
+        }
+        
     }
 }
