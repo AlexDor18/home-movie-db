c3786ed386df3788700cdfb3d73dfc6f0c3f4391
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-21 13:28:09 +0200

    test: fix test

--- home-movie-db-frontend/src/tests/components/OverviewTable/OverviewTable.test.tsx
+++ home-movie-db-frontend/src/tests/components/OverviewTable/OverviewTable.test.tsx
@@ -6,8 +6,8 @@ describe('OverviewTable', () => {
   it('renders the OverviewTable component', () => {
     render(<OverviewTable />);
     
-    expect(screen.getByText(/Title/))
-    expect(screen.getByText(/Beschreibung/))
+    expect(screen.getByText(/Title/)).toBeInTheDocument();
+    expect(screen.getByText(/Beschreibung/)).toBeInTheDocument();
   });
 
     it('renders data inserted in the table', () => {
