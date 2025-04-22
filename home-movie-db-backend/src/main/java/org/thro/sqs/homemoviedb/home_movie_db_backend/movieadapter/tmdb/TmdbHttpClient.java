ffabd79851d9a456189ba73c91451601f9b57bbd
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-22 11:04:21 +0200

    feat: fix environment environment

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbHttpClient.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbHttpClient.java
@@ -1,6 +1,5 @@
 package org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.tmdb;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
@@ -11,14 +10,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TmdbHttpClient {
 
-    //@Value("${tmdb.apiKey}")
-    private String tmdbApiKey;
+    private TmdbConfiguration configuration;
 
     private RestClient restClient;
 
-    public TmdbHttpClient(){
-        tmdbApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2UxODEwZTIwZDE5N2ZmYTIxNTRhM2JkNjE1NDZmNiIsIm5iZiI6MTU0NTU3NDY1My44NDgsInN1YiI6IjVjMWY5OGZkOTI1MTQxNjgwNWJmZTdhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VKSP8ZOqfQL9L8BP8mrNJ2_uLOhvEsldqbs9ikY4kLs";
-        restClient = RestClient.builder().baseUrl("https://api.themoviedb.org/3").defaultHeader("Authorization", "Bearer " + tmdbApiKey).defaultHeader("Content-Type", "application/json").build();
+    public TmdbHttpClient(TmdbConfiguration config) {
+        configuration = config;
+
+        log.info(config.getApiKey());
+
+        restClient = RestClient.builder().baseUrl("https://api.themoviedb.org/3").defaultHeader("Authorization", "Bearer " + configuration.getApiKey()).defaultHeader("Content-Type", "application/json").build();
     }
 
     public <T extends Object> T get(String uri, Class<T> responseClass) {
