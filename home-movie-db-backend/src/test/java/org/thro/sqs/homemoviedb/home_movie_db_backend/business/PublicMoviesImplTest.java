599a868b96dcc0362d193e33070231ffb884f0c0
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-22 11:39:23 +0200

    choire: fix build

--- home-movie-db-backend/src/test/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/PublicMoviesImplTest.java
+++ home-movie-db-backend/src/test/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/PublicMoviesImplTest.java
@@ -4,19 +4,36 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.business.models.MovieDTO;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.interfaces.MovieInformations;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PublicMoviesImplTest {
 
     @InjectMocks
     private PublicMoviesImpl sut;
 
+    @Mock
+    private MovieInformations movieInformationsMock;
+
     @Test
     void getAllMovies() {
+        Mockito.when(movieInformationsMock.getMovieInformationsById(ArgumentMatchers.anyLong())).thenReturn(new MovieDTO(){{
+            setId(1L);
+            setOverview("unittest");
+            setTitle("unittest");
+            setThumbnailUrl("");
+        }});
+
         final List<MovieDTO> result = this.sut.getAllMovies();
         Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("unittest", result.getFirst().getTitle());
     }
 }
