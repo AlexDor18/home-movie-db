ef66157bf515607ded1cb27225572bbca332c409
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-22 12:42:29 +0200

    tests: added test

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbHttpClient.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbHttpClient.java
@@ -22,6 +22,14 @@ public class TmdbHttpClient {
         restClient = RestClient.builder().baseUrl("https://api.themoviedb.org/3").defaultHeader("Authorization", "Bearer " + configuration.getApiKey()).defaultHeader("Content-Type", "application/json").build();
     }
 
+    /**
+     * Executes a get request to the TMDB API definied by the relative URI
+     * 
+     * @param uri The relative URI of the API endpoint to be called
+     * @param responseClass The class of the response body
+     * @return The response body
+     * @throws HttpClientErrorException if the API responded with an error
+     */
     public <T extends Object> T get(String uri, Class<T> responseClass) {
         try {
             return restClient.get().uri(uri).retrieve().body(responseClass);
---
+++ home-movie-db-backend/src/test/java/org/thro/sqs/homemoviedb/home_movie_db_backend/movieadapter/tmdb/TmdbMovieInformationsTest.java
@@ -0,0 +1,45 @@
+package org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.tmdb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.business.models.MovieDTO;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.tmdb.mapper.TmdbMapper;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.tmdb.models.TmdbMovieMessage;
+
+@ExtendWith(MockitoExtension.class)
+class TmdbMovieInformationsTest {
+
+    @Mock
+    private TmdbHttpClient tmdbHttpClientMock;
+
+    @Spy
+    private TmdbMapper tmdbMapper = Mappers.getMapper(TmdbMapper.class);
+
+    @InjectMocks
+    private TmdbMovieInformations sut;
+
+    @Test
+    void getMovieInformationsByIdTest() {
+        Mockito.when(tmdbHttpClientMock.get("/movie/1", TmdbMovieMessage.class)).thenReturn(new TmdbMovieMessage(){{
+            setId(1);
+            setOverview("overview");
+            setTitle("title");
+            setPoster_path("thumbnailUrl");
+        }});
+
+        MovieDTO result = sut.getMovieInformationsById(1L);
+
+        Assertions.assertEquals(1, result.getId());
+        Assertions.assertEquals("overview", result.getOverview());
+        Assertions.assertEquals("title", result.getTitle());
+        Assertions.assertEquals("thumbnailUrl", result.getThumbnailUrl());
+    }
+
+}
