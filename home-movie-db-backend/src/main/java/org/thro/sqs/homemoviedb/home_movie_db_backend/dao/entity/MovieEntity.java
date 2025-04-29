0373ebe18ca70899d83641eccf5818158d462e3a
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-29 11:30:17 +0200

    chore: update entities

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/dao/entity/MovieEntity.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/dao/entity/MovieEntity.java
@@ -1,5 +1,6 @@
 package org.thro.sqs.homemoviedb.home_movie_db_backend.dao.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
@@ -12,11 +13,21 @@ public class MovieEntity {
 
     @Id
     private Long id;
+    
     private String title;
+    
     private String overview;
+    
+    @Column(name = "thumbnail_uri")
     private String thumbnailUri;
+    
     private boolean adult;
+    
     private String releaseDate;
+
+    @Column(name = "original_language")
     private String originalLanguage;
+    
+    @Column(name = "original_title")
     private String originalTitle; 
 }
