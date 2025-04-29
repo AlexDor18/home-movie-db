55978ec7ddf1bff95c37e738726c99c27df974a4
Alexander Dormann <AlDo14.AD@gmail.com>
2025-04-29 13:49:12 +0200

    feat: changed business interface

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/interfaces/Movies.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/interfaces/Movies.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.thro.sqs.homemoviedb.home_movie_db_backend.business.models.MovieDTO;
 
-public interface PublicMovies {
+public interface Movies {
 
     public List<MovieDTO> getAllMovies();
 }
