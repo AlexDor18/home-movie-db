a8b5ed7405a9dbfd4600c173013697852d42ba6f
Alexander Dormann <AlDo14.AD@gmail.com>
2025-05-05 10:41:10 +0200

    test: fix test

--- home-movie-db-backend/src/test/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/MoviesImplTest.java
+++ home-movie-db-backend/src/test/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/MoviesImplTest.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.business.models.MovieDTO;
-import org.thro.sqs.homemoviedb.home_movie_db_backend.movieadapter.interfaces.MovieInformations;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.dao.interfaces.dao.MovieDao;
 
 @ExtendWith(MockitoExtension.class)
 class MoviesImplTest {
@@ -20,16 +19,16 @@ class MoviesImplTest {
     private MoviesImpl sut;
 
     @Mock
-    private MovieInformations movieInformationsMock;
+    private MovieDao movieDaoMock;
 
     @Test
     void getAllMovies() {
-        Mockito.when(movieInformationsMock.getMovieInformationsById(ArgumentMatchers.anyLong())).thenReturn(new MovieDTO(){{
+        Mockito.when(this.movieDaoMock.getAllMoviesForUser(1L)).thenReturn(List.of(new MovieDTO(){{
             setId(1L);
             setOverview("unittest");
             setTitle("unittest");
             setThumbnailUrl("");
-        }});
+        }}));
 
         final List<MovieDTO> result = this.sut.getAllUserMovies();
         Assertions.assertEquals(1, result.size());
