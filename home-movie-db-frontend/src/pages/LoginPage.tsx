022cf157c8f3ada95ff52d36145f0a51d45e70f2
Alexander Dormann <AlDo14.AD@gmail.com>
2025-05-18 17:18:05 +0200

    feat: changed login form

--- home-movie-db-frontend/src/pages/LoginPage.tsx
+++ home-movie-db-frontend/src/pages/LoginPage.tsx
@@ -26,10 +26,40 @@ const LoginPage = () => {
     }
 
     return (
-        <div>
-            <input type="text" placeholder="Username" value={user} onChange={(e) => setUser(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={() => handleLogin()}>Login</button>
+        <div className="flex-1 flex justify-center items-center">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                handleLogin();
+            }} className="mx-auto max-w-md p-8 bg-white rounded-lg shadow-lg">
+            <label className="block text-gray-700 font-bold mb-2">
+                Username:{" "}
+                <input 
+                    type="text" 
+                    placeholder="Username" 
+                    value={user} 
+                    onChange={(e) => setUser(e.target.value)} 
+                    className="bg-white border-solid border-[1px] border-[#ccc] w-full py-1.5 px-2" 
+                />
+            </label>
+            <br />
+            <label className="block text-gray-700 font-bold mb-2">
+                Password:{" "}
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    className="bg-white border-solid border-[1px] border-[#ccc] w-full py-1.5 px-2" 
+                />
+            </label>
+            <br />
+            <button 
+                type="submit" 
+                className="bg-[#3200ee] px-4 py-1.5 text-white"
+            >
+                Login
+            </button>
+        </form>
         </div>
     );
 }
