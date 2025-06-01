887593a0a9e263ffd70bb43fcdf374fbfb3c9687
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-01 11:44:34 +0200

    feat: changed header style

--- home-movie-db-frontend/src/components/Header/Header.tsx
+++ home-movie-db-frontend/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import UserInfo from '../UserInfo/UserInfo'
 export const Header: React.FC = () => {
 
   return (
-    <header className="bg-white border-b-2 border-b-[#ccc] border-b-solid px-8 py-4 shadow-md">
+    <header className="bg-white border-b-2 border-b-[#ccc] border-b-solid px-8 py-4 shadow-md flex">
       <h1 className="text-xl font-bold">Home Movie DB</h1>
       <UserInfo />
     </header>
--- home-movie-db-frontend/src/components/UserInfo/UserInfo.tsx
+++ home-movie-db-frontend/src/components/UserInfo/UserInfo.tsx
@@ -3,9 +3,13 @@ import { useGetUserQuery } from "../../redux/api/userApi";
 const UserInfo = () => {
     const { data: user } = useGetUserQuery();
 
+    if(user === undefined){
+        return <></>;
+    }
+
     return (
-        <div>
-            Hello {user?.username}
+        <div className="ml-auto">
+            Hallo {user?.surname}, {user?.prename}
         </div>
     );
 };
