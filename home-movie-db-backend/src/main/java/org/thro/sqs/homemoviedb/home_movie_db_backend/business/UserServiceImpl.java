deb7e06a5113b052c62cfdcffe11e78a1a04b349
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-10 18:32:20 +0200

    fix: don't allow multiple user have the same username

--- home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/UserServiceImpl.java
+++ home-movie-db-backend/src/main/java/org/thro/sqs/homemoviedb/home_movie_db_backend/business/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.business.interfaces.UserService;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.business.models.UserDto;
 import org.thro.sqs.homemoviedb.home_movie_db_backend.dao.interfaces.dao.UserDao;
+import org.thro.sqs.homemoviedb.home_movie_db_backend.exceptions.UserAlreadyExistsException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,6 +23,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto createNewUser(UserDto user) {
+        if(userDao.getUserByUsername(user.getUsername()) != null) {
+            throw new UserAlreadyExistsException("User with username " + user.getUsername() + " already exists");
+        }
+
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(16);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
