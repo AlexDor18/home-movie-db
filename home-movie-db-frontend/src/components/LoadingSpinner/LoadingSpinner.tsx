d7ffb2f4a12901852ec0a578ad6c263d0dffc2ed
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-10 15:11:44 +0200

    test: added loading spinner test

--- home-movie-db-frontend/src/components/LoadingSpinner/LoadingSpinner.tsx
+++ home-movie-db-frontend/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,8 +1,7 @@
 const LoadingSpinner: React.FC = () => {
     return (
-        <div className="flex justify-center">
+        <div data-testid="loading-spinner" className="flex justify-center">
             <div className="animate-spin inline-block w-8 h-8 border-4 rounded-full border-t-transparent border-b-transparent">
-                <span className="sr-only">Loading...</span>
             </div>
         </div>
     );
