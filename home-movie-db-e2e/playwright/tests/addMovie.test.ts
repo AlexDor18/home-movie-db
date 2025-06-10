27050b352ce670a94bd1278ce038e7902b14dc05
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-10 18:24:44 +0200

    test: add movie search test

---
+++ home-movie-db-e2e/playwright/tests/addMovie.test.ts
@@ -0,0 +1,20 @@
+import test, { expect } from "@playwright/test";
+
+test.describe("Test add new movie", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/search");
+    });
+
+    test('should display empty search form', async ({page}) => {
+        await expect(page.getByText('<- Zurück zur Startseite')).toBeVisible();
+        await expect(page.getByPlaceholder('Search...')).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Search' })).toBeVisible();
+        await expect(page.getByText('Geben Sie für die Suche einen Begriff in die Suchleiste ein')).toBeVisible();
+    })
+
+    test('should display search results', async ({page}) => {
+        await page.getByPlaceholder('Search...').fill('Star Wars');
+        await page.getByRole('button', { name: 'Search' }).click();
+        await expect(page.getByText('Star Wars')).toBeVisible();
+    })  
+});
