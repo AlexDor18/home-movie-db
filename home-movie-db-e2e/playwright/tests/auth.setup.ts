a9bd26c31f76583d1112ba458218f938459651a2
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-14 12:27:46 +0200

    feat: fix setup script

--- home-movie-db-e2e/playwright/tests/auth.setup.ts
+++ home-movie-db-e2e/playwright/tests/auth.setup.ts
@@ -2,9 +2,27 @@ import { test as setup } from '@playwright/test';
 import path from 'path';
 import fs from 'fs';
 
+const userFile = path.join(__dirname, '../.auth/user.json');
 const authFile = path.join(__dirname, '../.auth/auth.json');
 
 setup('authenticate', async ({ page }) => {
+    if (fs.existsSync(userFile)) {
+    console.log('Skipping user creation');
+    return;
+  }
+
+  await page.goto('/signup');
+  await page.getByPlaceholder('Vorname').fill('Playwright');
+  await page.getByPlaceholder('Nachname').fill('User');
+  await page.getByPlaceholder('Username').fill('playwrightuser');
+  await page.getByPlaceholder('Password').fill('password123');
+  await page.getByRole('button', { name: 'Registrieren' }).click();
+  await page.waitForURL('/');
+
+  // End of user creation steps.
+
+  await page.context().storageState({ path: userFile });
+
   // Perform authentication steps. Replace these actions with your own.
   await page.goto('/');
   await page.getByLabel('Username').fill('playwrightuser');
