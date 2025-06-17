326c3c22f43bf6e592b0752c801cbd0f6724fbe7
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-17 15:51:53 +0200

    docs: removed missing links

--- home-movie-db-docs/docusaurus.config.ts
+++ home-movie-db-docs/docusaurus.config.ts
@@ -85,12 +85,6 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
@@ -125,10 +119,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
