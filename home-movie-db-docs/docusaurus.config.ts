99508db0e35a243886882b49ac0edef1c57717c4
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-24 17:57:03 +0200

    docs: fix edit link

--- home-movie-db-docs/docusaurus.config.ts
+++ home-movie-db-docs/docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/AlexDor18/home-movie-db',
+            'https://github.com/AlexDor18/home-movie-db/tree/main/home-movie-db-docs/',
         },
         theme: {
           customCss: './src/css/custom.css',
