77758f14b90739c31a4d273a1b780d9ebdc17975
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-26 23:20:16 +0200

    test: added test

--- home-movie-db-frontend/src/tests/components/OverviewTable/OverviewTable.test.tsx
+++ home-movie-db-frontend/src/tests/components/OverviewTable/OverviewTable.test.tsx
@@ -3,12 +3,16 @@ import { describe, expect, it } from 'vitest';
 import OverviewTable from '../../../components/OverviewTable/OverviewTable';
 import { testData } from './testData';
 import { columns } from '../../../components/OverviewTable/columns';
+import { MemoryRouter } from 'react-router';
 
 describe('OverviewTable', () => {
   it('renders the OverviewTable component', () => {
     const movies = testData;
     
-    render(<OverviewTable columns={columns} data={movies}/>);
+    render(
+    <MemoryRouter initialEntries={['/home']}>
+      <OverviewTable columns={columns} data={movies}/>
+    </MemoryRouter>);
     
     expect(screen.getByText(/Title/)).toBeInTheDocument();
     expect(screen.getByText(/Beschreibung/)).toBeInTheDocument();
@@ -17,7 +21,10 @@ describe('OverviewTable', () => {
   it('renders data inserted in the table', () => {
     const movies = testData;    
 
-    render(<OverviewTable columns={columns} data={movies}/>);
+    render(
+    <MemoryRouter initialEntries={['/home']}>
+      <OverviewTable columns={columns} data={movies}/>
+    </MemoryRouter>);
 
     expect(screen.getByText(/The Shawshank Redemption/)).toBeInTheDocument();
   });
