1edb36aee705e4f9f705e4167f16b3e3f5aa803b
Alexander Dormann <AlDo14.AD@gmail.com>
2025-06-29 21:52:12 +0200

    doc: fix docu footer

--- home-movie-db-docs/docusaurus.config.ts
+++ home-movie-db-docs/docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Alexander Dormann. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
