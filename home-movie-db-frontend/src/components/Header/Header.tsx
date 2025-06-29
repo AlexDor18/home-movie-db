93aa43df62104a7c8e7e951790409aae204bd90c
Alexander Dormann <50829502+AlexDor18@users.noreply.github.com>
2025-06-29 20:11:10 +0200

    added icons (#40)

--- home-movie-db-frontend/src/components/Header/Header.tsx
+++ home-movie-db-frontend/src/components/Header/Header.tsx
@@ -8,6 +8,7 @@ export const Header: React.FC = () => {
 
   return (
     <header className="bg-white border-b-2 border-b-[#ccc] border-b-solid px-8 py-4 shadow-md flex">
+      <img alt="Home Movie DB Logo" src="/logo.png" className="w-8 h-8 mr-4" />
       <h1 className="text-xl font-bold">Home Movie DB</h1>
       <UserInfo user={data}/>
     </header>
