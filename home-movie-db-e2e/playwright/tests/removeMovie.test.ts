5a13246617d798ca61d064b9ecbedb87ce6c5e14
Alexander Dormann <AlDo14.AD@gmail.com>
2025-07-01 10:49:42 +0200

    test: specified e2e test

--- home-movie-db-e2e/playwright/tests/removeMovie.test.ts
+++ home-movie-db-e2e/playwright/tests/removeMovie.test.ts
@@ -8,7 +8,7 @@ test('test details page', async ({ page }) => {
   await page.getByRole('button', { name: 'Search' }).click();
   await page.getByRole('row', { name: 'Thumbnail Fluch der Karibik' }).getByRole('button').click();
   await page.getByRole('link', { name: '<- Zurück zur Startseite' }).click();
-  await page.getByRole('link', { name: 'Details' }).click();
+  await page.getByRole('row', { name: 'Thumbnail Fluch der Karibik' }).getByRole('link', { name: 'Details' }).click();
   await page.getByRole('button', { name: 'Entfernen' }).click();
   await expect(page.getByRole('button', { name: 'Hinzufügen' })).toBeVisible();
 });
